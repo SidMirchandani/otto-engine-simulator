c5177a144be76e959354b37cca36cb1e1a2a887e
Siddharth Mirchandani <88209108+SidMirchandani@users.noreply.github.com>
2025-03-13 22:39:55 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import OttoEngineSimulator from '../components/OttoEngineSimulator';
-
-export default function Home() {
-  return (
-    <main style={{padding: '2rem'}}>
-      <OttoEngineSimulator />
-    </main>
-  );
-}

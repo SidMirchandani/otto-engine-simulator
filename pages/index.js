5939d39f3ac438128c768bd0a7cdae8a0a8606fe
Siddharth Mirchandani <88209108+SidMirchandani@users.noreply.github.com>
2025-03-13 22:46:49 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import OttoEngineSimulator from '../components/OttoEngineSimulator';
+
+export default function Home() {
+  return (
+    <main style={{padding: '2rem'}}>
+      <OttoEngineSimulator />
+    </main>
+  );
+}
